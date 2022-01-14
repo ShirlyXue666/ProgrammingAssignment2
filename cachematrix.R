bc61694849096416e2bd07f85370bbaa4998fd5e
ShirlyXue666 <97705962+ShirlyXue666@users.noreply.github.com>
2022-01-13 21:42:02 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache potentially time-consuming computations, and in this case the computation is matrix inversion.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 
